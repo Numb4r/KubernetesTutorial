275da4e296f140581b0f490c02bfcf8d1aca3975
Numb4r <yuridimitre@protonmail.com>
2022-01-07 15:01:24 -0300

    Update script para criar apeans um arquivo

--- md2pdf.sh
+++ md2pdf.sh
@@ -4,5 +4,25 @@ TARGET="`pwd`/markdown"
 OUTPUT="`pwd`/main"
 TMP="/tmp"
 [ ! -d $OUTPUT ] && mkdir $OUTPUT
-ls $TARGET | grep .md | cut -d . -f 1 | xargs -t -I {} pandoc -f markdown $TARGET/{}.md -o $TMP/{}.html
-ls $TMP | grep .html | cut -d . -f 1 | xargs -t -I {} pandoc -f html $TMP/{}.html --pdf-engine=xelatex -V colorlinks  -o $OUTPUT/{}.pdf
+[ ! -d $TMP ] && mkdir $TMP
+ARRAY=( 
+    "${TARGET}/indice.md"
+    "${TARGET}/Nodes.md" 
+    "${TARGET}/ConfigMaps.md" 
+    "${TARGET}/PersistentVolume.md"
+    "${TARGET}/RollbackDeployment.md" 
+      )
+
+
+pandoc -f markdown ${ARRAY[@]} metadata.yaml -s -t html -o $TMP/main.html
+
+
+sed -i '/<header id="title-block-header">/,/<\/header>/d' $TMP/main.html # Comentar essa linha se for gerar um html 
+
+pandoc -f html $TMP/main.html --pdf-engine=xelatex -V colorlinks -t latex  -o $OUTPUT/main.pdf
+
+
+# ls $TARGET | grep .md | cut -d . -f 1 | xargs -t -I {} pandoc -f markdown $TARGET/{}.md -s -t html -o $TMP/{}.html
+
+# ls $html/ | grep .html | xargs -I {} sed -i "s/.md/.html/g" $html/{}
+# ls $TMP | grep .html | cut -d . -f 1 | xargs -t -I {} pandoc -f html $TMP/{}.html --pdf-engine=xelatex -V colorlinks -t latex  -o $OUTPUT/{}.pdf
